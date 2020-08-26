e585ba458f83dce6b622c76e67d0d3fcb795b6c7
kaleab tadewos ero <tadewoskaleab@gmail.com>
2020-08-26 11:05:20 -0500

    All about Location : Location Controller , Service , Mappers , Location Response.

---
+++ meditation-appointment-check/src/main/java/app_checking/controller/LocationController.java
@@ -0,0 +1,44 @@
+package app_checking.controller;
+
+import app_checking.domain.Location;
+import app_checking.dto.LocationResponse;
+import app_checking.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/locations")
+public class LocationController {
+
+    @Autowired
+    private LocationService locationService;
+
+    @PostMapping()
+    public LocationResponse createLocation(@RequestBody Location location) {
+    	return locationService.save(location);
+    }
+    
+    @GetMapping()
+    public List<LocationResponse> getLocations(){
+    	return locationService.findAll();
+    }
+    
+    @GetMapping("/{locationid}")
+    public LocationResponse getLocationById(@PathVariable int locationid) {
+    	return locationService.findLocationResponseById(locationid);
+    }
+    
+    @PutMapping()
+    public LocationResponse updateById(@RequestBody Location location) {
+    	return locationService.update(location);
+    }
+    
+    @DeleteMapping("/{locationid}")
+    public void deleteLocation(@PathVariable int locationid) {
+    	locationService.delete(locationid);
+    }
+    
+    
+}
--- meditation-appointment-check/src/main/java/app_checking/service/impl/LocationServiceImpl.java
+++ meditation-appointment-check/src/main/java/app_checking/service/impl/LocationServiceImpl.java
@@ -1,102 +1,102 @@
-//package app_checking.service.impl;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.transaction.annotation.Propagation;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import app_checking.domain.Location;
-//import app_checking.dto.LocationResponse;
-//import app_checking.repository.LocationRepo;
-//import app_checking.service.LocationService;
-//import app_checking.util.mapper.LocationResponseMapper;
-//
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.stream.Collectors;
-//
-//@Component
-//@Transactional
-//public class LocationServiceImpl implements LocationService {
-//	
-//	@Autowired
-//    LocationRepo locationRepository;
-//
-//	@Autowired
-//	protected LocationResponseMapper responseMapper;
-//
-//	public LocationResponse save(Location location) {
-//		
-//		locationRepository.save(location);
-//		return convertEntityToResponse(location);
-//	}
-//
-//	@Transactional(readOnly = true)
-//	public List<LocationResponse> findAll(){
-//		List<Location> locations = locationRepository.findAll();
-//		return convertEntityListToResponse(locations);
-//	}
-//
-//	@Transactional(readOnly = true)
-//	public LocationResponse findLocationResponseById(int locationid) {
-//		Optional<Location> location = locationRepository.findById(locationid);
-//		LocationResponse locationResponse = new LocationResponse();
-//		if(location.isPresent()) {
-//			Location location1 = location.get();
-//			return convertEntityToResponse(location1);
-//		}
-//		else {
-//			return null;
-//		}
-//	}
-//
-//	@Transactional(readOnly = true)
-//	public Location findById(int locationid) {
-//		Optional<Location> location = locationRepository.findById(locationid);
-//		return location.isPresent() ? location.get() : null;
-//	}
-//	
-//	public LocationResponse update(Location newlocation) {
-//		Location oldLocation = findById(newlocation.getId());
-//    	if(oldLocation == null){
-//    		return null;
-//    	}
-//    	oldLocation.setRoomNo(newlocation.getRoomNo());
-//    	oldLocation.setBuildNo(newlocation.getBuildNo());
-//    	oldLocation.setCity(newlocation.getCity());
-//    	oldLocation.setState(newlocation.getState());
-//    	oldLocation.setStreet(newlocation.getStreet());
-//    	oldLocation.setZipcode(newlocation.getZipcode());
-//    	locationRepository.save(oldLocation);
-//    	return convertEntityToResponse(oldLocation);
-//	}
-//	
-//	public void delete(int locationId) {
-//		Location oldLocation = findById(locationId);
-//    	if(oldLocation == null){
-//    		return;
-//    	}
-//    	locationRepository.deleteById(locationId);
-//	}
-//
-//	@Override
-//	@Transactional(propagation= Propagation.SUPPORTS)
-//	public List<LocationResponse> convertEntityListToResponse(List<Location> locationList) {
-//		if(null == locationList){
-//			return null;
-//		}
-//		else {
-//			return locationList.stream()
-//					.map(responseMapper::map)
-//					.collect(Collectors.toList());
-//		}
-//	}
-//
-//	@Override
-//	@Transactional(propagation=Propagation.SUPPORTS)
-//	public LocationResponse convertEntityToResponse(Location location) {
-//		return responseMapper.map(location);
-//	}
-//
-//}
+package app_checking.service.impl;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import app_checking.domain.Location;
+import app_checking.dto.LocationResponse;
+import app_checking.repository.LocationRepo;
+import app_checking.service.LocationService;
+import app_checking.util.mapper.LocationResponseMapper;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+@Transactional
+public class LocationServiceImpl implements LocationService {
+	
+	@Autowired
+    LocationRepo locationRepository;
+
+	@Autowired
+	protected LocationResponseMapper responseMapper;
+
+	public LocationResponse save(Location location) {
+		
+		locationRepository.save(location);
+		return convertEntityToResponse(location);
+	}
+
+	@Transactional(readOnly = true)
+	public List<LocationResponse> findAll(){
+		List<Location> locations = locationRepository.findAll();
+		return convertEntityListToResponse(locations);
+	}
+
+	@Transactional(readOnly = true)
+	public LocationResponse findLocationResponseById(int locationid) {
+		Optional<Location> location = locationRepository.findById(locationid);
+		LocationResponse locationResponse = new LocationResponse();
+		if(location.isPresent()) {
+			Location location1 = location.get();
+			return convertEntityToResponse(location1);
+		}
+		else {
+			return null;
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public Location findById(int locationid) {
+		Optional<Location> location = locationRepository.findById(locationid);
+		return location.isPresent() ? location.get() : null;
+	}
+	
+	public LocationResponse update(Location newlocation) {
+		Location oldLocation = findById(newlocation.getId());
+    	if(oldLocation == null){
+    		return null;
+    	}
+    	oldLocation.setRoomNo(newlocation.getRoomNo());
+    	oldLocation.setBuildNo(newlocation.getBuildNo());
+    	oldLocation.setCity(newlocation.getCity());
+    	oldLocation.setState(newlocation.getState());
+    	oldLocation.setStreet(newlocation.getStreet());
+    	oldLocation.setZipcode(newlocation.getZipcode());
+    	locationRepository.save(oldLocation);
+    	return convertEntityToResponse(oldLocation);
+	}
+	
+	public void delete(int locationId) {
+		Location oldLocation = findById(locationId);
+    	if(oldLocation == null){
+    		return;
+    	}
+    	locationRepository.deleteById(locationId);
+	}
+
+	@Override
+	@Transactional(propagation= Propagation.SUPPORTS)
+	public List<LocationResponse> convertEntityListToResponse(List<Location> locationList) {
+		if(null == locationList){
+			return null;
+		}
+		else {
+			return locationList.stream()
+					.map(responseMapper::map)
+					.collect(Collectors.toList());
+		}
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.SUPPORTS)
+	public LocationResponse convertEntityToResponse(Location location) {
+		return responseMapper.map(location);
+	}
+
+}
