ae547d86d4d8801f294730ac9df66ff8c8d712e2
kaleab tadewos ero <tadewoskaleab@gmail.com>
2020-08-29 15:17:38 -0500

    COMMENTED NOTE ABOUT INFERRED QUERIES , CUSTOM JPQL AND NATIVE QUERIES

--- meditation-appointment-check/src/main/java/app_checking/repository/ReservationRepository.java
+++ meditation-appointment-check/src/main/java/app_checking/repository/ReservationRepository.java
@@ -23,10 +23,33 @@ Note
 @Transactional
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 	
+	//Inferred Queries : UserEntity findByName(String name);
+	// queries like this are short so Spring Data know what to do. so we don't have to tell anything to it. it just create a query for us.
+	
+	//but for longer method name like the below queries : write your own custom JPQL query using @Query annotations.
+	
+	//Note that : Spring Data give us validity check as soon as you do this. If they are not write , you gonna have an exception. so they guaranted you are writing working query.
+	
+	
 	@Query(value = "SELECT consumer FROM Reservation re WHERE date(re.appointment.date) >= date(:date) AND date(re.appointment.date) <= date(:date)  AND re.status = 'ACCEPTED'")
 	public List<User> findAcceptedReservationsByAppointmentDate(@Param("date") LocalDate date);
 	
 	@Query(value="FROM Reservation re WHERE re.appointment.id = :app_id")
 	public List<Reservation> getAllReservationsByAppointmentId(@Param("app_id") int appointmentId);
+	
+	
+	//Note 
+	//Inferred Queries : Sample => UserEntity findByName(String name);
+	//Custom JPQL Queries with @Query : Sample => @Query("select u from UserEntity u where u.name = :name")
+	//											  UserEntity findByNameCustomQuery(@Param("name") String name);
+	//Native Queries with @Query : Sample => @Query(
+	//  											value = "select * from user as u where u.name = :name",
+	//  											nativeQuery = true)
+	//												UserEntity findByNameNativeQuery(@Param("name") String name);
+	
+	//For Native Queries :- Since the query may contain database-specific SQL, there’s no way Spring Data or Hibernate can know what to check for
+		//native queries may also differ among databases , so it may not work for all type of databases and also in memory databases.
+	
+	
 	
 }
