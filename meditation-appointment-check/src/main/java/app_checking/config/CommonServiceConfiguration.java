b8e00a88770d395f107a2d09ac152c94c93795ac
kaleab tadewos ero <tadewoskaleab@gmail.com>
2020-09-01 15:43:23 -0500

    CONFIGURE HTTPS

--- meditation-appointment-check/src/main/java/app_checking/config/CommonServiceConfiguration.java
+++ meditation-appointment-check/src/main/java/app_checking/config/CommonServiceConfiguration.java
@@ -1,5 +1,11 @@
 package app_checking.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +24,41 @@ public class CommonServiceConfiguration {
     @Bean
     public MapperFactory mapperFactory(){
         return new DefaultMapperFactory.Builder().build();
+    }
+    
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8081);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
     }
     
 }
